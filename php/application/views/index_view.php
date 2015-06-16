3f83918bd19ae5d27fbe61c10f454dab7baea6da
Wen Zhang <wen@click3x.com>
2015-06-16 15:59:45 -0400

    added favicon

--- php/application/views/index_view.php
+++ php/application/views/index_view.php
@@ -11,15 +11,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
         <!-- icons -->
-        <link rel="shortcut icon" href="<?php echo base_url(); ?>favicon.ico" type="image/x-icon" />
-        <link rel="apple-touch-icon" href="<?php echo base_url(); ?>apple-touch-icon.png" />
-        <link rel="apple-touch-icon" sizes="57x57" href="<?php echo base_url(); ?>apple-touch-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url(); ?>apple-touch-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url(); ?>apple-touch-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url(); ?>apple-touch-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="<?php echo base_url(); ?>apple-touch-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="<?php echo base_url(); ?>apple-touch-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="<?php echo base_url(); ?>apple-touch-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-touch-icon-76x76.png">
+        <link rel="icon" type="image/png" href="favicon/favicon-32x32.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="favicon/favicon-96x96.png" sizes="96x96">
+        <link rel="icon" type="image/png" href="favicon/favicon-16x16.png" sizes="16x16">
+        <link rel="manifest" href="img/manifest.json">
         <!-- end icons -->
 
         <link rel="stylesheet" href="<?php echo base_url(); ?>css/main.css">
@@ -86,9 +85,8 @@
                             <!-- <li><a data-navigate-to="home" href="<?php echo base_url(); ?>home"><h1>HOME</h1></a></li> -->
                             <li><a data-navigate-to="projects" href="<?php echo base_url(); ?>directors"><h1>DIRECTORS</h1></a></li>
                             <li><a data-navigate-to="projects" href="<?php echo base_url(); ?>projects"><h1>WORK</h1></a></li>
-                            <li><a data-navigate-to="about" href="<?php echo base_url(); ?>home"><h1>ABOUT</h1></a></li>
-                            <!-- <li><a data-navigate-to="people" href="<?php echo base_url(); ?>people" ><h1>PEOPLE</h1></a></li> -->
-                            <li><a href="http://www.click3x.com/contact"><h1>CONTACT</h1></a></li>
+                            <li><a data-navigate-to="about" href="<?php echo base_url(); ?>about"><h1>ABOUT</h1></a></li>
+                            <li><a href="<?php echo base_url(); ?>contact"><h1>CONTACT</h1></a></li>
                             <li><a href="http://www.click3x.com"><h1>CLICK 3X</h1></a></li>
                         </ul>
                     </div>
